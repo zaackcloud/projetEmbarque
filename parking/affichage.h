f5d10087ee6b69e23eab5bca87642d44c18dc3e9
aurelien <aurelien@DebianAjc.myguest.virtualbox.org>
2023-09-14 11:48:49 +0200

    first commit

---
+++ parking/affichage.h
@@ -0,0 +1,15 @@
+#ifndef AFFICHAGE_H
+#define AFFICHAGE_H
+
+
+class affichage
+{
+public:
+    affichage();
+    int accueil();
+private:
+
+    void navigation(int choix);
+};
+
+#endif // AFFICHAGE_H
---
+++ parking/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "affichage.h"
+
+
+using namespace std;
+
+int main()
+{
+    affichage *a=new affichage;
+    a->accueil();
+    return 0;
+}
